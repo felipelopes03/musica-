bc647a23bc027c2f9342ecb6080d3cf59816330d
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-05 20:18:49 -0300

    musiquinha

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿Console.Clear();
+
+Console.WriteLine("I feel so unsure");
+Console.WriteLine("As I take your hand and lead you to the dance floor");
+Console.WriteLine("As the music dies, something in your eyes");
+Console.WriteLine("Calls to mind a silver screen");
+Console.WriteLine("And all its sad good-byes");
+
+Console.WriteLine();
+
+
+Console.WriteLine("I'm never gonna dance again");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("Guilty feet have got no rhythm");
+Console.WriteLine("Though it's easy to pretend");
+Console.WriteLine("I know you're not a fool");
+Console.WriteLine("I should've known better than to cheat a friend");
+Console.WriteLine("And waste the chance that I'd been given");
+Console.WriteLine("So I'm never gonna dance again");
+Console.WriteLine("The way I danced with you, oh");
+Console.WriteLine("Time can never mend");
+Console.WriteLine("The careless whispers of a good friend");
+Console.WriteLine("To the heart and mind");
+Console.WriteLine("Ignorance is kind");
+Console.WriteLine("There's no comfort in the truth");
+Console.WriteLine("Pain is all you'll find");
+Console.WriteLine("I'm never gonna dance again");
+Console.WriteLine("Guilty feet have got no rhythm");
+Console.WriteLine("Though it's easy to pretend");
+Console.WriteLine("I know you're not a fool");
+
+Console.WriteLine();
+
+Console.WriteLine("I should've known better than to cheat a friend (should've known better, yeah)");
+
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("And waste the chance that I'd been given");
+Console.ResetColor();
+
+Console.WriteLine();
+
+Console.WriteLine("So I'm never gonna dance again");
+Console.WriteLine("The way I danced with you, oh");
+Console.WriteLine("Never without your love");
+Console.WriteLine("Tonight the music seems so loud");
+Console.WriteLine("I wish that we could lose this crowd");
+Console.WriteLine("Maybe it's better this way");
+Console.WriteLine("We'd hurt each other with the things we'd want to say");
+Console.WriteLine("We could have been so good together");
+Console.WriteLine("We could have lived this dance forever");
+Console.WriteLine("But now, who's gonna dance with me?");
+Console.WriteLine("Please stay");
+Console.WriteLine("And I'm never gonna dance again");
+Console.WriteLine("Guilty feet have got no rhythm");
+Console.WriteLine("Though it's easy to pretend");
+Console.WriteLine("I know you're not a fool");
+Console.WriteLine("I should've known better than to cheat a friend");
+
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("And waste the chance that I'd been given");
+Console.ResetColor();
+
+Console.WriteLine();
+
+Console.WriteLine("So I'm never gonna dance again");
+Console.WriteLine("The way I danced with you, oh");
+Console.WriteLine("now that you're gone");
+Console.WriteLine("(Now that you're gone) was what I did so wrong, so wrong");
+Console.WriteLine("That you had to leave me alone?");
+
+Console.WriteLine();
